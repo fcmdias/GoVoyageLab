d16aa3ce24aa9d0353176517bf416870ae7ab0cd
fcmdias <fernando.cesardias7@yahoo.com>
2022-09-18 15:27:18 +0100

    added crud features

--- golang_docker_mysql/main.go
+++ golang_docker_mysql/main.go
@@ -5,9 +5,23 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"net/http"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 )
+
+var version = "0.1.3"
+var db *sql.DB
+
+//Connect creates MySQL connection
+func Connect() error {
+	var err error
+		db, err = sql.Open("mysql", "tester:secret@tcp(db:3306)/test")	
+		if err != nil {
+			return err
+		}
+		return nil
+}
 
 type User struct {
 	ID   int    `json:"id"`
@@ -15,14 +29,6 @@ type User struct {
 }
 
 func getUsers() []*User {
-	// Open up our database connection.
-	db, err := sql.Open("mysql", "tester:secret@tcp(db:3306)/test")
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		log.Print(err.Error())
-	}
-	defer db.Close()
 
 	// Execute the query
 	results, err := db.Query("SELECT * FROM users")
@@ -45,83 +51,115 @@ func getUsers() []*User {
 	return users
 }
 
-func addUsers()  {
-	// Open up our database connection.
-	db, err := sql.Open("mysql", "tester:secret@tcp(db:3306)/test")
 
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		log.Print(err.Error())
-	}
-	defer db.Close()
+func getUser(w http.ResponseWriter, r *http.Request) {
 
+	params := mux.Vars(r)
+  result, err := db.Query("SELECT id, name FROM users WHERE id = ?", params["id"])
+  if err != nil {
+    panic(err.Error())
+  }
+  defer result.Close()
+  var user User
+  for result.Next() {
+    err := result.Scan(&user.ID, &user.Name)
+    if err != nil {
+      panic(err.Error())
+    }
+  }
+	json.NewEncoder(w).Encode(user)
 
+}
 
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  stmt, err := db.Prepare("DELETE FROM users WHERE id = ?")
+  if err != nil {
+    panic(err.Error())
+  }
+  _, err = stmt.Exec(params["id"])
+ if err != nil {
+    panic(err.Error())
+  }
+fmt.Fprintf(w, "User with ID = %s was deleted", params["id"])
+}
 
+func updateUser(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  stmt, err := db.Prepare("UPDATE users SET name = ? WHERE id = ?")
+  if err != nil {
+    panic(err.Error())
+  }
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err.Error())
+  }
+  keyVal := make(map[string]string)
+  json.Unmarshal(body, &keyVal)
+  newName := keyVal["name"]
+  _, err = stmt.Exec(newName, params["id"])
+  if err != nil {
+    panic(err.Error())
+  }
+  fmt.Fprintf(w, "User with ID = %s was updated with name: %s", params["id"], newName)
+}
 
-
-	/// start 
-
-
-	sql := "INSERT INTO users(name) VALUES ('John Doe')"
-	res, err := db.Exec(sql)
-
-	if err != nil {
-			panic(err.Error())
-	}
-
-	lastId, err := res.LastInsertId()
-
-	if err != nil {
-			log.Fatal(err)
-	}
-
-	fmt.Printf("The last inserted row id: %d\n", lastId)
-
-
-	/// end 
-	// Execute the query
-	// result, err := db.Query("SELECT * FROM users")
-	// if err != nil {
-	// 	panic(err.Error()) // proper error handling instead of panic in your app
-	// }
-
-	// var users []*User
-	// for results.Next() {
-	// 	var u User
-	// 	// for each row, scan the result into our tag composite object
-	// 	err = results.Scan(&u.ID, &u.Name)
-	// 	if err != nil {
-	// 		panic(err.Error()) // proper error handling instead of panic in your app
-	// 	}
-
-	// 	users = append(users, &u)
-	// }
-
-	// return users
+func createUser(w http.ResponseWriter, r *http.Request) {
+  stmt, err := db.Prepare("INSERT INTO users(name) VALUES(?)")
+  if err != nil {
+    panic(err.Error())
+  }
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err.Error())
+  }
+  keyVal := make(map[string]string)
+  json.Unmarshal(body, &keyVal)
+  name := keyVal["name"]
+  _, err = stmt.Exec(name)
+  if err != nil {
+    panic(err.Error())
+  }
+  fmt.Fprintf(w, "New user was created")
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the HomePage! version %s", version)
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func userPage(w http.ResponseWriter, r *http.Request) {
+func errorPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Error connecting to db %s", globalError.Error())
+	fmt.Println("Error connecting to db %s", globalError.Error())
+}
+
+func usersPage(w http.ResponseWriter, r *http.Request) {
 	users := getUsers()
 	
 	fmt.Println("Endpoint Hit: usersPage")
 	json.NewEncoder(w).Encode(users)
 }
-func adduserPage(w http.ResponseWriter, r *http.Request) {
-	addUsers()
-	
-	fmt.Println("Endpoint Hit: adduserPage")
-	fmt.Fprintf(w, "users added!")
-}
+
+var globalError error
 
 func main() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/users", userPage)
-	http.HandleFunc("/users/add", adduserPage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	globalError = Connect()
+
+	router := mux.NewRouter()
+	if globalError != nil {
+		http.HandleFunc("/", errorPage)
+	} else {
+		router.HandleFunc("/", homePage).Methods("GET")
+
+		router.HandleFunc("/user", createUser).Methods("POST")
+		router.HandleFunc("/user/{id}", getUser).Methods("GET")
+		router.HandleFunc("/user/{id}", updateUser).Methods("PUT")
+		router.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
+		router.HandleFunc("/users", usersPage).Methods("GET")
+		
+
+		defer db.Close()
+	}
+	http.ListenAndServe(":8080", router)
 }
