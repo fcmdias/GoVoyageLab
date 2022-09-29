719dd5bc926964f4ec15f126b2bd591b94afa8d7
fcmdias <fernando.cesardias7@yahoo.com>
2022-09-29 16:36:48 +0100

    multiple year compound interest calculation

--- savings_rate_calculator/main.go
+++ savings_rate_calculator/main.go
@@ -8,6 +8,7 @@ type Account struct {
 
 func (a *Account) Deposit(v float64) {
 	a.Amount += v
+	a.Amount = rounddown(a.Amount)
 }
 
 func (a Account) Cal() float64 {
@@ -22,4 +23,20 @@ func (a Account) Cal() float64 {
 	}
 
 	return math.Floor(result*100) / 100
+}
+
+func (a *Account) MultipleYearCal(y int) float64 {
+
+	var res float64
+	for i := 1; i < y; i++ {
+		v := a.Cal()
+		res += v
+		a.Deposit(v)
+	}
+	res += a.Cal()
+	return res
+}
+
+func rounddown(v float64) float64 {
+	return math.Floor(v*100) / 100
 }
--- savings_rate_calculator/main_test.go
+++ savings_rate_calculator/main_test.go
@@ -2,6 +2,58 @@ package main
 
 import "testing"
 
+func TestAccount_MultipleYearCal(t *testing.T) {
+	type Test struct {
+		initialamount float64
+		expected      float64
+		years         int
+	}
+	var tests = []Test{
+		Test{
+			100,
+			2.01,
+			2,
+		},
+		Test{
+			1000,
+			50.6,
+			2,
+		},
+		Test{
+			1000,
+			20,
+			1,
+		},
+		Test{
+			100,
+			1,
+			1,
+		},
+		Test{
+			3000,
+			90,
+			1,
+		},
+		Test{
+			3000,
+			376.52,
+			4,
+		},
+		Test{
+			1000,
+			330.82,
+			10,
+		},
+	}
+
+	for _, test := range tests {
+		a := Account{test.initialamount}
+		result := a.MultipleYearCal(test.years)
+		if test.expected != result {
+			t.Fatalf("test failed. amount: %v, result: %v, expected: %v", a.Amount, result, test.expected)
+		}
+	}
+}
 func TestAccount_Cal(t *testing.T) {
 	type Test struct {
 		amount             float64
