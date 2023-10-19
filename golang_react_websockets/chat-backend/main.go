c8db29f8473746a139738049be1653e472c3fa2a
fcmdias <fernando.cesardias7@yahoo.com>
2023-10-19 17:13:47 +0100

    broadcasting from different users

--- golang_react_websockets/chat-backend/main.go
+++ golang_react_websockets/chat-backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"sync"
@@ -14,7 +15,13 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-var clients = make(map[*websocket.Conn]bool)
+type Client struct {
+	Conn     *websocket.Conn
+	Group    string
+	Username string
+}
+
+var clients = make(map[Client]bool)
 var lock = sync.Mutex{}
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
@@ -23,16 +30,29 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Error upgrading connection:", err)
 		return
 	}
-	defer func() {
-		lock.Lock()
-		delete(clients, conn)
-		lock.Unlock()
-		conn.Close()
-	}()
+
+	group := r.URL.Query().Get("group")
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		username = "anonymous"
+	}
+	client := Client{Conn: conn, Group: group, Username: username}
 
 	lock.Lock()
-	clients[conn] = true
+	clients[client] = true
 	lock.Unlock()
+
+	// Broadcast updated user list to the group
+	broadcastUsers(group)
+
+	defer func() {
+		lock.Lock()
+		delete(clients, client)
+		lock.Unlock()
+		conn.Close()
+		// Broadcast updated user list to the group after user disconnects
+		broadcastUsers(group)
+	}()
 
 	for {
 		messageType, msg, err := conn.ReadMessage()
@@ -41,22 +61,67 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		broadcast(messageType, msg)
+		var message map[string]string
+		json.Unmarshal(msg, &message)
+		switch message["type"] {
+		case "message":
+			message["content"] = message["content"] + " from " + client.Username
+			bresponse, err := json.Marshal(message)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("Message received:", message["content"])
+			broadcast(messageType, bresponse)
+		default:
+			broadcastUsers(group)
+			log.Println("Unknown message type:", message["type"])
+		}
 	}
 }
 
 func broadcast(messageType int, msg []byte) {
+	log.Println("broadcasting message")
 	lock.Lock()
 	defer lock.Unlock()
 
 	for client := range clients {
-		err := client.WriteMessage(messageType, msg)
+		err := client.Conn.WriteMessage(messageType, msg)
 		if err != nil {
 			log.Printf("Error broadcasting message to client: %v", err)
-			client.Close()
+			client.Conn.Close()
 			delete(clients, client)
 		}
 	}
+}
+
+func broadcastUsers(group string) {
+	users := getUsersForGroup(group)
+	message, _ := json.Marshal(map[string]interface{}{
+		"type":  "users_list",
+		"users": users,
+	})
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	for client := range clients {
+		if client.Group == group {
+			client.Conn.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
+
+func getUsersForGroup(group string) []string {
+	lock.Lock()
+	defer lock.Unlock()
+
+	var usernames []string
+	for client := range clients {
+		if client.Group == group {
+			usernames = append(usernames, client.Username)
+		}
+	}
+	return usernames
 }
 
 func main() {
--- golang_react_websockets/chat-frontend/src/components/chat/Chat.js
+++ golang_react_websockets/chat-frontend/src/components/chat/Chat.js
@@ -3,32 +3,63 @@ import React, { useState, useEffect } from "react";
 function Chat() {
     const [ws, setWs] = useState(null);
     const [messages, setMessages] = useState([]);
+    const [usersList, setUsersList] = useState([]);
 
+
+    const group = "teachers";
+    const username = "sky" + Math.random().toString(36).substring(2, 7);
+
+    
     useEffect(() => {
-        const websocket = new WebSocket("ws://localhost:8080/ws");
+        const websocket = new WebSocket(`ws://localhost:8080/ws?group=${group}&username=${username}`);
+
         websocket.onmessage = (event) => {
-            setMessages((prevMessages) => [...prevMessages, event.data]);
+            const message = JSON.parse(event.data);
+
+            switch (message.type) {
+                case "message":
+                    console.log("setting message: ", message);
+                    setMessages((prevMessages) => [...prevMessages, message]);
+                    console.log("messages: ", messages);
+                    break;
+                case "users_list":
+                    setUsersList(message.users);
+                    break;
+                // ... handle other message types if needed
+            }
         };
+
         setWs(websocket);
         return () => websocket.close();
     }, []);
 
     const sendMessage = (message) => {
-        ws && ws.send(message);
+        console.log("sending message: ", message);
+        ws && ws.send(JSON.stringify({
+            type: "message",
+            content: message,
+        }));
     };
-
-    return (
+return (
+    <div>
+        <h1>Chat</h1>
+        <span>Username: {username}</span>   
+        <h3>Online Users:</h3>
+        <ul>
+            {usersList.map((user) => (
+                <li key={user}>{user}</li>
+            ))}
+        </ul>
         <div>
-            <div>
-                {messages.map((msg, index) => (
-                    <p key={index}>{msg}</p>
-                ))}
-            </div>
-            <button onClick={() => sendMessage("hello" + new Date().toString())}>
-                Send Message
-            </button>
+            {messages.map((msg, index) => (
+                <p key={index}>{msg.content}</p>
+            ))}
         </div>
-    );
-}
+        <button onClick={() => sendMessage("hello " + new Date().toString())}>
+            Send Message
+        </button>
+    </div>
+);
+            }
 
 export default Chat;
